01a575d3328db3bd09e00a0fed93631982a1847e
Daniel Slone <slonej3@nku.edu>
2016-01-12 15:06:41 -0500

    Initial Commit

---
+++ slone_james_prog1-362/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  slone_james_prog1-362
+//
+//  Created by James Slone on 1/12/16.
+//  Copyright © 2016 James Slone. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
